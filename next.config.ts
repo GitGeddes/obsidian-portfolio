568c6a21a25b3a50f5d8b7ca136c52972db7895e
GitGeddes <hellohead102@gmail.com>
2025-07-30 21:57:01 -0700

    Update nextjs.yml

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import remarkGfm from "remark-gfm";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ['md', 'mdx', 'ts', 'tsx'],
   experimental: {
     mdxRs: true
   }
